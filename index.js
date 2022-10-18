a03e93b4e1ef5e59202485387e54ff9198b0a757
DiptaPal <diptapal1998@gmail.com>
2022-10-18 19:02:06 +0600

    basic backend sever for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+const categories = require('./data/categories.json');
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send('New API Running')
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port , () => {
+    console.log(`Dragon News Server running on port: ${port}`);
+})
